3305e468ba4599a343398ccdad6deda7d1cb15ad
Karalynn Partain <78173292+kpartain@users.noreply.github.com>
2021-09-29 15:20:51 -0700

    initial commit

---
+++ fundamentals/Calories.playground/Contents.swift
@@ -0,0 +1,14 @@
+var user = "Michael Scott"
+var calories = 2400
+
+var alsoUser: String = "Michael Scott"
+var alsoCalories: Int = 2400
+
+print(user)
+print(calories)
+
+user = "Dwight Schrute"
+print(user)
+
+let unableToChange = "Don't change this"
+let unableToMutate = 2000
